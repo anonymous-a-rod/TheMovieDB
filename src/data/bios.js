d6f83bfa36efff251c77efb3f3bc555cd0e6efcf
Aaron <aaron1venema@gmail.com>
2023-02-26 20:49:02 -0600

    bio

--- src/data/bios.js
+++ src/data/bios.js
@@ -8,7 +8,17 @@ const teamMembers = [
         github: "https://github.com/anonymous-a-rod",
         linkedIn: "https://www.linkedin.com/in/aaron-venema-a4797354/",
         id: 1,
-    }
+    },
+    {
+        name: "Kelsey Jackson",
+        pictureURL: "/assets/kelsey.jpg",
+        bio: "I am a passionate individual focused on front end web development. I love to use code to bring out my inner creativity. Self-learning is a trait I've embraced and I enjoy absorbing as much as I can to bring out the best in myself and others.",
+        website: "portfolio-project",
+        websiteHref: "https://portfolio-react-project.netlify.app/",
+        github: "https://github.com/kelso540",
+        linkedIn: "https://www.linkedin.com/in/kelsey-jackson-22b098256/",
+        id: 2,
+    },
 ]
 
 export default teamMembers;
