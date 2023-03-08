125b537275b2914394852caedc64ae82d0953c46
Kelsey Jackson <kelsojacks@outlook.com>
2023-03-07 23:55:07 -0500

    finished arrows but havent styled yet

--- src/components/PreviewRow.jsx
+++ src/components/PreviewRow.jsx
@@ -1,52 +1,72 @@
-import { useEffect, useState } from "react";
 import SelectionCard from "./SelectionCard";
 import Spinner from "./Spinner";
-import { BsArrowRightSquare, BsArrowLeftSquare } from 'react-icons/bs';
+import React, { useState, useEffect } from 'react';
+import { BsArrowLeftSquare, BsArrowRightSquare } from 'react-icons/bs';
 
 const PreviewRow = ({category, searchQuery, additionalQuery}) => {
-    const [loading, setLoading] = useState(true);
-    const [selections, setSelections] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selections, setSelections] = useState([]);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [showLeft, setShowLeft] = useState(false);
+  const [showRight, setShowRight] = useState(true);
 
+  useEffect(() => {
+    setLoading(true);
+    try {
+      fetch(`https://api.themoviedb.org/3/${searchQuery}?api_key=09cbcde820a19e4959494fa25a97a645${additionalQuery}`)
+        .then((response) => response.json())
+        .then((data) => setSelections(data.results))
+    } catch(error) {
+      console.log(error)
+    }
+    setLoading(false);
+  }, [searchQuery, additionalQuery]);
 
+  const handleScrollRight = () => {
+    const container = document.getElementById(category);
+    const containerWidth = container.offsetWidth;
+    container.scrollBy({ left: containerWidth, behavior: 'smooth' });
+    setScrollPosition(container.scrollLeft + containerWidth);
+  };
 
-    useEffect(()=>{
-        setLoading(true);
-        try{
-            fetch(`${`https://api.themoviedb.org/3/${searchQuery}?api_key=09cbcde820a19e4959494fa25a97a645${additionalQuery}`}`)
-                .then((response) => response.json())
-                .then((data) => setSelections(data.results))
-        } catch(error){
-            console.log(error)
-        }
-        setLoading(false);
-    },[searchQuery, additionalQuery])
+  const handleScrollLeft = () => {
+    const container = document.getElementById(category);
+    const containerWidth = container.offsetWidth;
+    container.scrollBy({ left: -containerWidth, behavior: 'smooth' });
+    setScrollPosition(container.scrollLeft - containerWidth); 
+    console.log(scrollPosition);
+  };
 
-    // console.log(selections)
+  const handleScroll = () => {
+    const container = document.getElementById(category);
+    const maxScrollPosition = container.scrollWidth - container.offsetWidth;
+    setShowLeft(container.scrollLeft > 0);
+    setShowRight(container.scrollLeft < maxScrollPosition);
+  };
 
-
-    return ( 
-        <>
-            { loading && <Spinner /> }
-            { !loading && 
-            <div className="w-screen flex flex-col items-center relative">
-                <h2 className="text-4xl font-bold capitalize mb-4 w-full text-left">{category}</h2>
-                <div className='w-full flex overflow-scroll overflow-y-hidden my-5 border-2 z-10'>
-                <BsArrowRightSquare className="absolute right-0 top-1/2 -translate-y-1/2 z-20 h-20 w-20" />
-                <BsArrowLeftSquare className="absolute left-0 top-1/2 -translate-y-1/2 z-20 h-20 w-20" />
-                { selections?.length > 0 && selections.map((selection)=>
-                    (
-                        <div key={selection.id}>
-                            <div>
-                                <SelectionCard selection={selection} />
-                            </div>
-                        </div>     
-                    )
-                )}
-                </div>
-            </div>
-            }      
-        </>
-     );
+  return ( 
+    <>
+      { loading && <Spinner /> }
+      { !loading && 
+        <div className="w-screen flex flex-col items-center relative">
+          <h2 className="text-4xl font-bold capitalize mb-4 w-full text-left">{category}</h2>
+          <div className='w-full flex overflow-scroll overflow-y-hidden my-5 border-2 z-10' id={category} onScroll={handleScroll}>
+            <BsArrowRightSquare className={showRight ? "absolute right-0 top-1/2 -translate-y-1/2 z-20 h-20 w-20" : "hidden"} onClick={handleScrollRight} />
+            <BsArrowLeftSquare className={showLeft ? "absolute left-0 top-1/2 -translate-y-1/2 z-20 h-20 w-20" : "hidden"} onClick={handleScrollLeft} />
+            { selections?.length > 0 && selections.map((selection) =>
+              (
+                <div key={selection.id}>
+                  <div>
+                    <SelectionCard selection={selection} />
+                  </div>
+                </div>     
+              )
+            )}
+          </div>
+        </div>
+      }      
+    </>
+  );
 }
  
 export default PreviewRow;
