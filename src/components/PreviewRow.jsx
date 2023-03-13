cb2790c80f0e9c6c998484731238f9c9c9b5bbd1
Aaron <aaron1venema@gmail.com>
2023-03-13 15:11:03 -0500

    Update PreviewRow.jsx

--- src/components/PreviewRow.jsx
+++ src/components/PreviewRow.jsx
@@ -50,7 +50,7 @@ const PreviewRow = ({category, searchQuery, additionalQuery}) => {
       { !loading && 
         <div className="w-screen flex flex-col items-center relative">
           <h2 className="text-4xl font-bold capitalize mt-4 w-full text-left ml-20">{category}</h2>
-          <div className='w-full flex overflow-auto overflow-y-hidden my-4 z-10 p-2' id={category} onScroll={handleScroll} >
+          <div className='w-full flex overflow-auto overflow-y-hidden my-4 z-10 p-2 gap-x-6' id={category} onScroll={handleScroll} >
             <BsFillArrowRightSquareFill className={showRight ? "absolute right-5 top-1/2 -translate-y-1/4 z-20 w-0 h-0 text-red-700 text-opacity-80 cursor-pointer hover:opacity-90 sm:h-20 sm:w-20" : "hidden"} onClick={handleScrollRight} />
             <BsFillArrowLeftSquareFill className={showLeft ? "absolute left-5 top-1/2 -translate-y-1/4 z-20 w-0 h-0 text-red-700 text-opacity-80 cursor-pointer hover:opacity-90 sm:h-20 sm:w-20" : "hidden"} onClick={handleScrollLeft} />
             { selections?.length > 0 && selections.map((selection) =>
