e9483170bf23acec1a731cedc86bcbb74b209e69
Aaron <aaron1venema@gmail.com>
2023-03-12 19:16:32 -0500

    Update CastCrew.jsx

--- src/components/CastCrew.jsx
+++ src/components/CastCrew.jsx
@@ -8,7 +8,7 @@ const CastCrew = ({info, title, showMoreInfo}) => {
     return ( 
         <div className={(info.length >= 0)?'relative mb-10 max-w-6xl mx-auto':'hidden'}>
             <h3 className={"text-3xl ml-10 mb-10 w-full capitalize"}>{title}</h3>
-            <div className="mx-auto max-w-4xl min-w-min grid grid-cols-1 md:grid-cols-2 gap-8 overflow-hidden mb-2 justify-items-start items-start">
+            <div className="mx-auto max-w-lg md:max-w-4xl min-w-min grid grid-cols-1 md:grid-cols-2 gap-8 overflow-hidden mb-2 justify-items-start items-start">
             {info.map((item, index) => {
                 if (!showMore && index > 3) {
                 return <></>;
@@ -21,7 +21,7 @@ const CastCrew = ({info, title, showMoreInfo}) => {
                         key={item.name + index}
                         className={
                         item.profile_path !== null
-                            ? "flex flex-row justify-start items-center pl-8 sm:pl-24 md:pl-12"
+                            ? "flex flex-row justify-start items-center pl-24 md:pl-12  lg:pl-18"
                             : "hidden"
                         }
                     >
